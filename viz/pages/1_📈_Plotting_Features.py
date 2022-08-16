e102ecf276334c6f41c8cd28e4fde1c0430eec99
huuthieu <huuthieucqtq@gmail.com>
2022-08-16 17:33:53 +0700

    Add streamlit pages

---
+++ viz/pages/1_📈_Plotting_Features.py
@@ -0,0 +1,132 @@
+import plotly.graph_objects as go
+import streamlit as st
+
+from models import *
+from utils.get_data import get_data
+from utils.plot import *
+from models.models import convert_label
+
+st.set_page_config(page_title="Plotting Features", page_icon="📈")
+
+st.markdown("# Plotting Features")
+st.sidebar.header("Plotting Features")
+st.write("""This demo will visualize the data of each province. Enjoy!""")
+
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    option1 = st.selectbox("Choose year", tuple(list_years[1:]))
+
+with col2:
+    option2 = st.selectbox("Choose province", tuple(["All"] + list_provinces))
+
+with col3:
+    option3 = st.selectbox("Choose feature", tuple(list_features))
+
+df = get_data()
+
+
+st.header(f"{option3} Chart in 5 years")
+if option2 == "All":
+    df_prov = df
+else:
+    df_prov = df[df["Province"] == option2]
+
+genre = st.radio("Visualization in", options=("Annualy", "Monthly"))
+
+if genre == "Annualy":
+    fig = plot_5y_annualy(df_prov, option3)
+else:
+    fig = plot_5y_monthly(df_prov, option3)
+st.plotly_chart(fig)
+
+
+df = df[df["Year"] == int(option1)].reset_index(drop=True)
+
+if option2 == "All":
+    st.header(f"{option3} of Viet Nam in {option1}")
+else:
+    st.header(f"{option3} of {option2} vs national average")
+genre = st.radio("Type of visualization", ("Monthly", "Daily"))
+
+if genre == "Monthly":
+    with st.spinner("Wait for it..."):
+        fig = compareWithNationalInMonth(df, option2, option3)
+else:
+    with st.spinner("Wait for it..."):
+        fig = compareWithNationalInDay(df, option2, option3)
+
+st.plotly_chart(fig)
+
+
+if option2 == "All":
+    st.header(f"Maximum and Minimum of {option3} in Viet Nam")
+else:
+    st.header(f"Maximum and Minimum of {option3} in {option2}")
+
+genre1 = st.radio("Choose type of visualization", ("Monthly", "Daily"))
+
+if genre1 == "Monthly":
+    with st.spinner("Wait for it..."):
+        fig = PLotMinMaxMonth(df, option2, option3)
+else:
+    with st.spinner("Wait for it..."):
+        fig = PLotMinMaxDay(df, option2, option3)
+
+st.plotly_chart(fig)
+
+
+st.header(f"Distribution of {option3}")
+if option2 == "All":
+    data = df[option3]
+else:
+    data = df[df.Province == option2][option3]
+fig = plotCountAndDistribution(data, option3)
+with st.spinner("Wait for it..."):
+    st.plotly_chart(fig)
+
+
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+
+df["Label"] = df["Weather"].apply(convert_label)
+
+values = df["Label"].value_counts().values
+labels = df["Label"].unique().tolist()
+
+
+st.header(f"Ratio of Rain and No Rain Weather")
+
+colors = ["darkorange", "blue"]
+
+
+fig = go.Figure(
+    go.Pie(
+        labels=["No Rain", "Rain"],
+        values=values,
+        hoverinfo="value",
+        textinfo="percent",
+        marker=dict(colors=colors, line=dict(color="#000000", width=2)),
+    )
+)
+
+fig.update_layout(
+    width=800, font=dict(family="Courier New, monospace", size=18)
+)
+
+
+st.plotly_chart(fig)
+
+
+st.header(f"Correlation between all features")
+
+df = df.drop(columns=["Year"])
+# df = df.replace("Label", {"No Rain":0.0, "Rain":1.0})
+
+fig = plt.figure(figsize=(15, 10))
+sns.heatmap(df.corr(), cmap="Greens", annot=True)
+st.write(fig)
+
+
+st.button("Re-run")
---
+++ viz/pages/4_📈_Weather_Forecast.py
@@ -0,0 +1,146 @@
+import datetime
+import re
+import time
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+import requests
+import streamlit as st
+from bs4 import BeautifulSoup
+from plotly.subplots import make_subplots
+
+from models.models import *
+from models.preprocess import *
+from cfg import *
+from utils.format import rounding_time
+from utils.get_data import get_data, get_data_msn
+
+st.set_page_config(page_title="Forecast", page_icon="📈")
+
+st.markdown("# Forecast")
+st.sidebar.header("Forecast")
+st.write(
+    """This is a demo of our weather forecast dashboard. We will display current weather values and make predictions for the near future. Enjoy!"""
+)
+
+option = st.selectbox("Choose province", tuple(list_provinces))
+
+province = pd.read_excel(province_path)
+province_dict = dict(zip(province.columns, province.iloc[0]))
+
+
+url = "https://www.worldweatheronline.com/Can-Tho-weather/vn.aspx"
+url = url.replace("Can-Tho", province_dict[option])
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "html.parser")
+
+now = datetime.datetime.now()
+
+st.markdown(
+    f"<p style='color: #FF5733; "
+    f"font-weight: bold; font-size: 25px;'> 📍 Weather at {option} currently </p>",
+    unsafe_allow_html=True,
+)
+
+with st.spinner("Wait for it..."):
+    temperature = (
+        soup.find_all("div", {"class": "weather-widget-temperature"})[0]
+        .find_all("p")[0]
+        .text.strip()
+    )
+    # wind_speed = soup.find_all("span", {"class": "wind-speed"})[0].text
+    weather = (
+        soup.find_all("div", {"class": "weather-widget-icon"})[0]
+        .find_all("p")[0]
+        .text.strip()
+    )
+    feats = soup.find_all("div", {"class": "ws-details-item"})
+    wind_speed = feats[0].find_all("span")[0].text.strip()
+    dir = feats[0].find_all("img")[0]["style"]
+    dir = re.findall("\(.*\)", dir)[0].strip("(deg)")
+    rain = feats[1].find_all("span")[0].text.strip()
+    cloud = feats[2].find_all("span")[0].text.strip()
+    humidity = feats[3].find_all("span")[0].text.strip()
+    pressure, dew_point = get_data_msn(option)
+    # a = soup.find_all("div",{"class":"wind-direction"})[0].find_all("img")[0]
+    # dir = re.findall("\(.*\)",a["style"])[0].strip('(deg)')
+    # st.markdown(f"#### ☁️ Weather: {weather}")
+    col1, col2 = st.columns(2)
+    col1.markdown("#### " + now.strftime("%d/%m/%Y"))
+    col1.markdown("##### " + now.strftime("%H:%M"))
+    col2.metric("☁️ Weather", weather)
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        col1.metric("🌡️ Temperature", f"{temperature}°C")
+        col1.metric("💨 Wind Speed", f"{wind_speed}")
+    with col2:
+        col2.metric("💧 Rain", f"{rain}")
+        col2.metric("💨 Wind Dir", f"{dir}°")
+
+    with col3:
+        col3.metric("💦 Humidity", f"{humidity}")
+        col3.metric("🕗 Pressure", f"{pressure}")
+    with col4:
+        col4.metric("☁️ Cloud", f"{cloud}")
+        col4.metric("🌡️ Dew Point", f"{dew_point}")
+
+# st.markdown("![](https://www.worldweatheronline.com/staticv150817/assets-202110/img/wind-direction.svg)")
+# st.metric("☔ Rain Alert", "Rain")
+st.markdown(
+    f"<p style='color: #FF5733; "
+    f"font-weight: bold; font-size: 25px;'> 📍 Alert in next 3 hours </p>",
+    unsafe_allow_html=True,
+)
+
+df = pd.read_csv(dataset_path)
+# genre = st.radio(
+#     "",
+#      options = ('Annualy', 'Monthly'))
+gust = 0.0
+feature_vals = [
+    humidity,
+    dew_point,
+    temperature,
+    rain,
+    cloud,
+    wind_speed,
+    dir,
+    pressure,
+    gust,
+    weather,
+]
+print(feature_vals)
+
+dict_test = dict(zip(list_features, feature_vals))
+
+time_h = rounding_time(now.hour)
+if len(str(time_h)) == 1:
+    time = "0" + str(time_h) + ":00"
+else:
+    time = str(time_h) + ":00"
+
+dict_test.update(
+    {
+        "Day": int(now.strftime("%d")),
+        "Month": int(now.strftime("%m")),
+        "Year": int(now.strftime("%Y")),
+        "Time": time,
+    }
+)
+
+
+data_test = pd.DataFrame([dict_test])
+
+data_test = process_data(data_test)
+
+res = pipeline(df, data_test, option)
+
+if res < 0.5:
+    label = "No Rain"
+else:
+    label = "Rain"
+st.metric("☔ Rain Alert", f"{label}")
+# st.markdown("### ☔ Rain Alert in next 3 hours: Rain")
