0479d5073c28d5e3ac0d4dfc57028898dbb55eed
huuthieu <huuthieucqtq@gmail.com>
2022-08-16 12:44:18 +0700

    Add models

---
+++ viz/models/cfg.py
@@ -0,0 +1,18 @@
+RAINY_LABEL = [
+    "Light drizzle",
+    "Light rain",
+    "Light rain shower",
+    "Patchy light drizzle",
+    "Patchy light rain",
+    "Patchy light rain with thunder",
+    "Patchy rain possible",
+    "Heavy rain",
+    "Heavy rain at times",
+    "Moderate or heavy rain shower",
+    "Moderate rain",
+    "Moderate rain at times",
+    "Overcast",
+    "Torrential rain shower",
+]
+RAINY_MONTH = [5, 6, 7, 8, 9, 10, 11]
+MORNING_HOUR = [6, 9, 12, 15]
